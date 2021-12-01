5c5dc2eb5fb9886fcb6cb7c581b2aa6540e46d13
Lucas Cardoso de Castro <sky.lucas2013@gmail.com>
2021-11-30 23:34:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import api from "./api";
+import "./app.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    artigos: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get("/listar-artigos");
+
+    console.log(response.data);
+
+    this.setState({ artigos: response.data });
+  }
+
+  render() {
+    const { artigos } = this.state;
+
+    return (
+      <div className="app">
+        <h1>Lista de artigos</h1>
+        <ul>
+          {artigos.map((artigo) => (
+            <li key={artigo._id}>
+              <h2>Título: {artigo.titulo}</h2>
+              <p>Conteúdo: {artigo.conteudo}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
